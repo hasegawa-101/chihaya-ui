6de739ed6fce7c36246d5b42aed79f5d8337fea0
hasegawa-101 <hasegawa@101de-sign.com>
2023-10-28 22:30:23 +0900

    feat: Removed base style.

--- src/components/Button/PrimaryButton/index.css.ts
+++ src/components/Button/PrimaryButton/index.css.ts
@@ -1,9 +1,5 @@
 import { style } from "@vanilla-extract/css";
 
 export const baseButtonStyle = style({
-  appearance: "none",
-  border: "none",
-  backgroundColor: "transparent",
-  padding: 0,
-  margin: 0,
+  lineHeight: "0",
 });
